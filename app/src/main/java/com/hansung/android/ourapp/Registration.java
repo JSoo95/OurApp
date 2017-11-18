6b041c0e668820eae70d3af12cabf57f5205ac18
taeyoung <yt2211@naver.com>
2017-11-18 22:22:49 +0900

    카메라버튼 눌렀을 때 카메라앱 실행 후, 촬영사진 으로 이미지 대체

--- app/src/main/java/com/hansung/android/ourapp/Registration.java
+++ app/src/main/java/com/hansung/android/ourapp/Registration.java
@@ -1,7 +1,21 @@
 package com.hansung.android.ourapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Registration extends AppCompatActivity {
 
@@ -9,5 +23,59 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        ImageButton btn = (ImageButton)findViewById(R.id.iconItem);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
     }
+
+    String mPhotoFileName;
+    File mPhotoFile;
+
+    final int REQUEST_IMAGE_CAPTURE = 100;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+            if (mPhotoFile !=null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.hansung.android.ourapp", mPhotoFile);
+
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else
+                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+                ImageButton imageView = (ImageButton)findViewById(R.id.iconItem);
+                imageView.setImageURI(Uri.fromFile(mPhotoFile));
+                //mAdapter.addItem(new MediaItem(MediaItem.SDCARD, mPhotoFileName, Uri.fromFile(mPhotoFile), MediaItem.IMAGE));
+            }
+        }
+    }
+
+
+
 }
